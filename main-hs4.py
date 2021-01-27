d7ba3cf3991b1b4fe0a30a1f554e2d43b1b377cd
mikew <waugh.e.michael@gmail.com>
2021-01-27 10:43:52 -0500

    december update

--- main-hs4.py
+++ main-hs4.py
@@ -91,7 +91,7 @@ def make_plot():
     # This is standard bokeh stuff so far
         plot = figure(x_axis_type="datetime", plot_height = height, plot_width=width, toolbar_location = 'below',
            tools = "box_zoom, reset, pan, xwheel_zoom", title = title,
-                  x_range = (dt.datetime(2017,7,1),dt.datetime(2021,1,1)) )
+                  x_range = (dt.datetime(2017,7,1),dt.datetime(2021,2,1)) )
 
         plot.line(x = x,
               y = y, line_width=3.5, line_alpha=0.75, line_color = "slategray")
@@ -100,7 +100,7 @@ def make_plot():
         
         plot = figure(x_axis_type="datetime", plot_height = height, plot_width=width, toolbar_location = 'below',
                tools = "box_zoom, reset, pan", title = title,
-                  x_range = (dt.datetime(2020,1,1),dt.datetime(2021,1,1)) )
+                  x_range = (dt.datetime(2020,1,1),dt.datetime(2021,2,1)) )
 
         plot.line(x = x,
                   y = y2017, line_width=3.5, line_alpha=0.5, line_color = "red", line_dash = "dashed"
